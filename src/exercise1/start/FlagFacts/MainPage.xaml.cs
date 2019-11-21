147d89283b827f80d47d7575c02cb12e79109bcc
Sibusiso Labi <skylasbu@gmail.com>
2019-11-21 13:23:04 +0200

    Mismatched Value Converters

--- src/exercise1/start/FlagFacts/MainPage.xaml.cs
+++ src/exercise1/start/FlagFacts/MainPage.xaml.cs
@@ -47,7 +47,6 @@ namespace FlagFacts
             country.SetBinding(Picker.SelectedItemProperty,
                        new Binding(nameof(CurrentFlag.Country)));
 
-            flagImage.Source = CurrentFlag.GetImageSource();
             /*
             adopted.Date = CurrentFlag.DateAdopted;
             adopted.DateSelected += (s, e) => CurrentFlag.DateAdopted = e.NewDate;
